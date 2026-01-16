5dd9ad404826f544d175ab17a363a9d731097603
ROSHINTH Sojan <142890245+Rosi8870@users.noreply.github.com>
2026-01-16 09:42:34 +0530

    backend servcer changed

--- billing-backend/server.js
+++ billing-backend/server.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const connectDB = require("./config/db");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+/* ---------- DB ---------- */
 connectDB();
 
-app.use("/api/invoices", require("./routes/invoice.routes"));
-app.use("/api/products", require("./routes/product.routes"));
-app.use("/api/customers", require("./routes/customer.routes"));
-app.use("/api/reports", require("./routes/report.routes"));
+/* ---------- MIDDLEWARE ---------- */
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://billpro.vercel.app"
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+
+/* ---------- ROUTES ---------- */
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/customers", require("./routes/customer.routes"));
+app.use("/api/products", require("./routes/product.routes"));
+app.use("/api/invoices", require("./routes/invoice.routes"));
+app.use("/api/reports", require("./routes/report.routes"));
 
-
+/* ---------- SERVER ---------- */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
